287ff5cb65e4fd544c8a2631ec082e45dd00d7ee
Harvey Limbo <limboh27@gmail.com>
2026-01-09 15:16:28 -0800

    add code to draw paddles and ball and code up paddle movement

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,135 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+#include <assert.h>
+#include <iostream>
+
 #include "print_hi.h"
 
 static SDL_Window* window = NULL;
 static SDL_Renderer* renderer = NULL;
 
+class Vector2
+{
+public:
+  float x, y;
+  explicit Vector2(): x(0.0f), y(0.0f) {}
+  explicit Vector2(float _x, float _y): x(_x), y(_y) {}
+  Vector2(const Vector2& other): x(other.x), y(other.y) {}
+  ~Vector2() {}
+
+  // operator overloading
+  Vector2 operator+(const Vector2& other) const 
+  {
+    return Vector2(x + other.x, y + other.y);
+  }
+  
+  Vector2 operator-(const Vector2& other) const 
+  {
+    return Vector2(x - other.x, y - other.y);
+  }
+
+  Vector2 operator*(const Vector2& other) const 
+  {
+    return Vector2(x * other.x, y * other.y);
+  }
+
+  Vector2 operator*(const float& scale) const
+  {
+    return Vector2(scale * x, scale * y);
+  }
+
+  Vector2 operator+=(const Vector2& other)
+  {
+    *this = *this + other;
+    return *this;
+  }
+
+  Vector2 operator-=(const Vector2& other)
+  {
+    *this = *this - other;
+    return *this;
+  }
+
+  Vector2 operator*=(const float& scale)
+  {
+    x *= scale;
+    y *= scale;
+    return *this;
+  }
+
+  Vector2 operator/=(const float& scale)
+  {
+    assert(scale != 0.0f && "Vector2 cannot divide by zero");
+    x /= scale;
+    y /= scale;
+    return *this;
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const Vector2& v)
+  {
+    os << "Vector2(" << v.x << ", " << v.y << ")";
+    return os;
+  }
+};
+
+// supports operations like 4.0f * Vector2(2.0f, 3.0f);
+Vector2 operator*(float scalar, const Vector2& other)
+{
+  return Vector2(scalar * other.x, scalar * other.y);
+}
+
+class Rectangle
+{
+public:
+  Vector2 position;
+  Vector2 dimension;
+  Vector2 velocity;
+
+  Rectangle() {}
+  Rectangle(const Rectangle& other): position(other.position), dimension(other.dimension), velocity(other.velocity) {}
+  ~Rectangle() {}
+
+  bool isOverlapping(const Rectangle& other)
+  {
+    bool isXOverlap = position.x <= other.position.x && other.position.x <= position.x + dimension.x;
+    bool isYOverlap = position.y <= other.position.y && other.position.y <= position.y + dimension.y;
+
+    return isXOverlap || isYOverlap;
+  }
+};
+
+static Rectangle paddle1;
+static Rectangle paddle2;
+static Rectangle ball;
+
+static bool isP1UpPressed = false;
+static bool isP1DownPressed = false;
+static bool isP2UpPressed = false;
+static bool isP2DownPressed = false;
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
 // Runs once on game startup
+// for appstate I can initialize it on the heap to add like OpenGL context
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
   HiSpace::printHi();
-  bool isCreated = SDL_CreateWindowAndRenderer("Hello World", 800, 600, NULL, &window, &renderer);
+  bool isCreated = SDL_CreateWindowAndRenderer("Hello World", SCREEN_WIDTH, SCREEN_HEIGHT, NULL, &window, &renderer);
   if (!isCreated)
   {
     SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
+
+  paddle1.dimension = Vector2(20.0f, 80.0f);
+  paddle2.dimension = Vector2(20.0f, 80.0f);
+  ball.dimension = Vector2(20.0f, 20.0f);
+
+  paddle1.position = Vector2(20.0f, (SCREEN_HEIGHT - paddle1.dimension.y) * 0.5f);
+  paddle2.position = Vector2(SCREEN_WIDTH - 40.0f, (SCREEN_HEIGHT - paddle1.dimension.y) * 0.5f);
+  ball.position = Vector2((SCREEN_WIDTH - ball.dimension.x) * 0.5f, (SCREEN_HEIGHT - ball.dimension.y) * 0.5f);
 
   return SDL_APP_CONTINUE;
 }
@@ -24,20 +138,109 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 // Runs when a new event occurs like input events
 SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 {
-  if (event->type == SDL_EVENT_KEY_DOWN || event->type == SDL_EVENT_QUIT)
+  if (event->type == SDL_EVENT_QUIT)
   {
     return SDL_APP_SUCCESS;
+  }
+
+  if (event->type == SDL_EVENT_KEY_DOWN)
+  {
+    SDL_Keycode key = event->key.key;
+    switch (key)
+    {
+      case SDLK_W:
+        isP1UpPressed = true;
+        break;
+      case SDLK_S:
+        isP1DownPressed = true;
+        break;
+      case SDLK_UP:
+        isP2UpPressed = true;
+        break;
+      case SDLK_DOWN:
+        isP2DownPressed = true;
+        break;
+    }
+  }
+
+  if (event->type == SDL_EVENT_KEY_UP)
+  {
+    SDL_Keycode key = event->key.key;
+    switch (key)
+    {
+      case SDLK_W:
+        isP1UpPressed = false;
+        break;
+      case SDLK_S:
+        isP1DownPressed = false;
+        break;
+      case SDLK_UP:
+        isP2UpPressed = false;
+        break;
+      case SDLK_DOWN:
+        isP2DownPressed = false;
+        break;
+    }
   }
 
   return SDL_APP_CONTINUE;
 }
 
 
+#define PADDLE_SPEED 4.0f
+
 // Runs every frame
 SDL_AppResult SDL_AppIterate(void* appstate)
 {
+
+  Vector2 up(0.0f, PADDLE_SPEED);
+  Vector2 down = -1.0f * up;
+
+  // move logic
+  if (isP1UpPressed)
+  {
+    paddle1.velocity = up;
+  }
+  else if (isP1DownPressed)
+  {
+    paddle1.velocity = down;
+  }
+  else
+  {
+    paddle1.velocity = Vector2(0.0f, 0.0f);
+  }
+
+  if (isP2UpPressed)
+  {
+    paddle2.velocity = up;
+  }
+  else if (isP2DownPressed)
+  {
+    paddle2.velocity = down;
+  }
+  else
+  {
+    paddle2.velocity = Vector2(0.0f, 0.0f);
+  }
+
+  // multiply by -1 because y coordinates are inverted
+  paddle1.position += paddle1.velocity * -1.0f;
+  paddle2.position += paddle2.velocity * -1.0f;
+
+
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
+
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  
+  SDL_FRect renderRects[3] {
+    SDL_FRect { paddle1.position.x, paddle1.position.y, paddle1.dimension.x, paddle1.dimension.y },
+    SDL_FRect { paddle2.position.x, paddle2.position.y, paddle2.dimension.x, paddle2.dimension.y },
+    SDL_FRect { ball.position.x, ball.position.y, ball.dimension.x, ball.dimension.y }
+  };
+  
+  SDL_RenderRects(renderer, renderRects, 3);
+
   SDL_RenderPresent(renderer);
 
 
@@ -45,8 +248,11 @@ SDL_AppResult SDL_AppIterate(void* appstate)
 }
 
 // Runs once on game exit
+// if I have appstate initialized on the heap, here is the area where I can delete any resources that the appstate has that is
+// also managed on the heap AND then delete appstate afterwards (like a destructor in C++)
 void SDL_AppQuit(void* appstate, SDL_AppResult result)
 {
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
+  HiSpace::printHi();
+  //SDL_DestroyRenderer(renderer);
+  //SDL_DestroyWindow(window);
 }
